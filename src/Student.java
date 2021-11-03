0ed2936645b58a840f3823f692b5a87822638f20
Stanislav Rakitov <prosperousrf@gmail.com>
2021-11-03 15:37:35 +0300

    Student class fields

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,12 @@
-/**
- * @author Stanislav Rakitov
- */
+/** @author Stanislav Rakitov */
 public class Student {
+
+  private String firstName;
+  private String lastName;
+  private String gradeYear;
+  private int studentID;
+  private String courses;
+  private int tuitionBalance;
+  private final int COSTOFCOURSE = 600;
+
 }
