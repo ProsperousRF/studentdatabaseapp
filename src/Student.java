379321ab4c5b698735c47dd473f9c4ea9b3bc14c
Stanislav Rakitov <prosperousrf@gmail.com>
2021-11-03 15:47:52 +0300

    Student class constructor

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+
 /** @author Stanislav Rakitov */
 public class Student {
 
+  private final int COSTOFCOURSE = 600;
   private String firstName;
   private String lastName;
-  private String gradeYear;
+  private int gradeYear;
   private int studentID;
   private String courses;
   private int tuitionBalance;
-  private final int COSTOFCOURSE = 600;
 
+  public Student() {
+    // This is a shitty code that instructor teaches us.
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter Student first name: ");
+    this.firstName = in.nextLine();
+    System.out.print("Enter Student last name: ");
+    this.lastName = in.nextLine();
+    System.out.print(
+        "1 - Freshmen\n"
+            + "2 - Sophmore\n"
+            + "3 - Junior\n"
+            + "4 - Senior\n"
+            + "Enter Student class level: ");
+    this.gradeYear = in.nextInt();
+    System.out.println(firstName + " " + lastName + " " + gradeYear);
+  }
 }
--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -2,5 +2,7 @@
  * @author Stanislav Rakitov
  */
 public class StudentDatabaseApp {
-
+  public static void main(String[] args) {
+    Student s1 = new Student();
+  }
 }
